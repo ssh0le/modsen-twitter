4f08c07841d43b9ea00d67b7363205b555c04fef
Bo <omuqqz@gmail.com>
2023-10-04 22:31:22 +0300

    init: configure project

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'react-refresh',
+    'simple-import-sort',
+  ],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^@?\\w'],
+          ['@/styles/\\w*.css$', '^@/', '^@appTypes/', '^@helpers'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\u0000', '^\\./\\w'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
